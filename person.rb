adbf5f82dbae590c8b671025504e5c48112191df
BryteLitty <littyofficials@gmail.com>
2023-02-16 02:36:14 +0000

    fix linter error

--- person.rb
+++ person.rb
@@ -6,8 +6,8 @@ class Person < Nameable
   attr_accessor :name, :age
 
   def initialize(age, parent_permission: true, name: 'Unknown')
+    super(name)
     @id = Random.rand(1..1000)
-    @name = name
     @age = age
     @parent_permission = parent_permission
   end
