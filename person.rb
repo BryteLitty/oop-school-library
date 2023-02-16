ccc65ab2f4ea3161f5c2f31c4b9e31820aaa8f3f
BryteLitty <littyofficials@gmail.com>
2023-02-16 03:39:42 +0000

    fix linter errors

--- person.rb
+++ person.rb
@@ -5,7 +5,7 @@ class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name: 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @age = age
@@ -14,7 +14,7 @@ class Person < Nameable
   end
 
   def can_use_services?
-    is_of_age? || @parent_permission
+    of_age? || @parent_permission
   end
 
   def correct_name
